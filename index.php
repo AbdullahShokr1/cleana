ece5d3b43a535375892549418a01fc7977484784
AbdullahShokr1 <darkthe254@gmail.com>
2024-01-02 01:23:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* Main Theme Files
+*
+* @package Cleana
+*/
+
+?>
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cleana Theme</title>
+</head>
+<body>
+    
+</body>
+</html>
